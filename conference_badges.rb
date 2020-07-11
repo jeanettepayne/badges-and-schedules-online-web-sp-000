5638368bf93ed231154828e71f2a5a36cc9d6a0d
Jeanette Payne <jeanetterpayne@gmail.com>
2020-07-11 15:35:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+# speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(speakers)
+speakers.each do |speaker| 
+  badges = badge_maker(speaker)
+ end
+badges
+end
+
+def assign_rooms
+  
+end
